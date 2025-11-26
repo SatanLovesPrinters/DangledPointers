52068ec5cec1aebcb56d60612eaba631e83f2f48
Antonio <antpin98@gmail.com>
2025-11-26 06:39:08 -0900

    Update MOOC2025_Intro_Part1.py

--- Python/BW - MOOC 2025 Python - Helsinki/MOOC2025_Intro_Part1.py
+++ Python/BW - MOOC 2025 Python - Helsinki/MOOC2025_Intro_Part1.py
@@ -1,5 +1,7 @@
 # https://programming-25.mooc.fi/part-1
 
+# getting started
+
 print (":-)")
 
 greeting = "Hello there!"
@@ -36,3 +38,5 @@ story_name = input("What is the story-be-tolder's name?: ")
 story_year = input("What year does this story take place?: ")
 story_general = input("Explain the story!: ")
 story_complete = print(f"The story begins with {story_name} on a warm day in the year {story_year}. {story_general}")
+
+# more about variables (str, int, float)
