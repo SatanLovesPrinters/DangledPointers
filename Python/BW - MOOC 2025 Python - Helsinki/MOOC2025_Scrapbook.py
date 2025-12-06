df4365ea18d239e37649a21e44a5567a16e3bec7
Antonio <antpin98@gmail.com>
2025-12-05 15:18:01 -0900

    Update MOOC2025_Scrapbook.py

--- Python/BW - MOOC 2025 Python - Helsinki/MOOC2025_Scrapbook.py
+++ Python/BW - MOOC 2025 Python - Helsinki/MOOC2025_Scrapbook.py
@@ -1,7 +1,27 @@
 # https://programming-25.mooc.fi/part-3
+# WIP | Need to get the columns to alternate between result output. 
 
 def chessboard(length):
-    pass   
+    row = length
+    column = length
+    row_results = ""
+    index = 0
+    column_index = 0
 
+    while column_index < length: 
+               
+        while index < length:
+            
+            if index % 2 == 0:
+                row_results += "1"
+                index += 1
+            else:
+                row_results += "0"
+                index += 1
+        
+        print(f"{row_results}")
+        column_index += 1
+
+            
 if __name__ == "__main__":
-    chessboard(1)
+    chessboard(6)
