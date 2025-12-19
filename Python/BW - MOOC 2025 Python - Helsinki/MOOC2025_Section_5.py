f7eea82336c15b2c8cc189b61c310d0d1ac48f55
Antonio <antpin98@gmail.com>
2025-12-18 19:04:51 -0900

    Sudoku Prep

--- Python/BW - MOOC 2025 Python - Helsinki/MOOC2025_Section_5.py
+++ Python/BW - MOOC 2025 Python - Helsinki/MOOC2025_Section_5.py
@@ -99,7 +99,7 @@ def block_correct(sudoku: list[list[int]], row_no:int, column_no:int) -> bool:
     for column in range(column_no, column_no + 3):
         for row in range(row_no, row_no + 3):
             value = sudoku[row][column]
-            if value > 0:
+            if value > 0: 
                 if value in values:
                     return False
                 values.append(value)
