7a3cbbd14b11b78274d24e616cf523f0b1eb05ca
Antonio <antpin98@gmail.com>
2025-12-22 06:18:19 -0900

    PyCharm env config + CodeWars scrap.py

--- Python/BW - MOOC 2025 Python - Helsinki/scrap.py
+++ Python/BW - MOOC 2025 Python - Helsinki/scrap.py
@@ -1,5 +1,28 @@
-def remove_exclamation_marks(s):
+def solution(s:str):
+    sNew = []
+    filler = ["_"]
+    results = []
+    sNew = s.split()
+    incr = 2
+    together = []
+    i = 0
 
-    return s.replace('!','')
+    if len(s) % 2 == 1:
+        combined = sNew + filler
+        together = "".join(combined)
 
-print(remove_exclamation_marks("Hello!"))
+        while i < len(together):
+            results.append(together[i:i+2])
+            i+=2
+        return results
+
+    elif len(s) % 2 == 0:
+        while i < len(s):
+            results.append(s[i:i + 2])
+            i += 2
+        return results
+    else:
+        return 0
+
+print(solution('abc'))
+print(solution('abcdef'))
