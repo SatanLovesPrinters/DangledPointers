68d01af63c11640f003ac63f058eb32c72da3451
Antonio <antpin98@gmail.com>
2025-12-22 19:47:31 -0900

    Update scrap.py

--- Python/BW - MOOC 2025 Python - Helsinki/scrap.py
+++ Python/BW - MOOC 2025 Python - Helsinki/scrap.py
@@ -1,11 +1,11 @@
 from collections import Counter
-def count_2(s):
-    if len(s) == 0:
-        return {}
-    else:
-        for i in s:
-        counts += Counter(letter)
-        return counts
+
+def delete_nth(order,max_e):
+    c = Counter(order)
+    counts = [c]
+    val_list = list(counts)
+    print(val_list)
+   
 
 
-print(count_2('aba'))
+delete_nth([20, 37, 20, 21], 1)
