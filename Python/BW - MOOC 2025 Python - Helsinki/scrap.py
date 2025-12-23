6f6579f20002797af1b26a7ef4b9e06c340ce1f9
Antonio <antpin98@gmail.com>
2025-12-22 16:51:12 -0900

    Update scrap.py

--- Python/BW - MOOC 2025 Python - Helsinki/scrap.py
+++ Python/BW - MOOC 2025 Python - Helsinki/scrap.py
@@ -1,17 +1,11 @@
-def count(s):
-    #counting with no OOP
-    alphabetStrLower = ['a','b','c','d','e','f','g','h','i',
-                          'j','k','l','m','n','o','p','q','r',
-                             's','t','u','v','w','x','y','z']
-
-    alphabetStrUpper = ['A','B','C','D','E','F','G','H','I',
-                          'J','K','L','M','N','O','P','Q','R',
-                             'S','T','U','V','W','X','Y','Z']
-    counter = 0
-    for letter in alphabetStrLower:
-        if letter in s:
-            counter = alphabetStrLower.count(letter)
+from collections import Counter
+def count_2(s):
+    if len(s) == 0:
+        return {}
+    else:
+        for i in s:
+        counts += Counter(letter)
+        return counts
 
 
-
-print(count('aba'))
+print(count_2('aba'))
