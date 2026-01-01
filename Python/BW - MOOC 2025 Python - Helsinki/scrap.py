d599acaf07ce0b480f8d6454d6bf4e21300182da
Antonio <antpin98@gmail.com>
2025-12-31 21:55:16 -0900

    extension case challenge (bad idea)

---
+++ Python/BW - MOOC 2025 Python - Helsinki/scrap.py
@@ -0,0 +1,26 @@
+def main():
+    extension = input("File name: ")
+    extensions(extension)
+
+
+def extensions(extension):
+    extension.lstrip(".")
+
+    match extension:
+        case ".gif":
+            print("image/gif")
+        case ".jpg" | ".jpeg":
+            print("image/jpeg")
+        case ".png":
+            print("image/png")
+        case ".pdf":
+            print("application/pdf")
+        case ".txt":
+            print("text/plain")
+        case ".zip":
+            print("application/zip")
+        case _:
+            print("application/octet-stream")
+
+
+main()
